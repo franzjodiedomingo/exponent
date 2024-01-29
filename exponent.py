0de008f2533b912cb7436663ca29ca0187fe97b4
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-29 21:43:12 +0800

    test

--- exponent.py
+++ exponent.py
@@ -1,3 +1,13 @@
 # Calculates base raised to the power of exp
 # Loop structure
 
+def exponent(base, exp):
+    result = 1
+    for _ in range(exp): 
+        result *= base
+    return result
+
+base = 2
+exp = 5
+result = exponent(base, exp)
+print(f"{base}^{exp} = {result}")
